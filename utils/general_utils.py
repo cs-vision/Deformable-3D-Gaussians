2051d1e1c51c68fd30da29e2f526ccd3d201a0aa
cs-vision <minoru981212@gmail.com>
2024-05-31 16:34:16 +0200

    fixed pickling error

--- utils/general_utils.py
+++ utils/general_utils.py
@@ -39,6 +39,42 @@ def ArrayToTorch(array, resolution):
         return resized_image_torch.unsqueeze(dim=-1).permute(2, 0, 1)
 
 
+# def get_expon_lr_func(
+#         lr_init, lr_final, lr_delay_steps=0, lr_delay_mult=1.0, max_steps=1000000
+# ):
+#     """
+#     Copied from Plenoxels
+
+#     Continuous learning rate decay function. Adapted from JaxNeRF
+#     The returned rate is lr_init when step=0 and lr_final when step=max_steps, and
+#     is log-linearly interpolated elsewhere (equivalent to exponential decay).
+#     If lr_delay_steps>0 then the learning rate will be scaled by some smooth
+#     function of lr_delay_mult, such that the initial learning rate is
+#     lr_init*lr_delay_mult at the beginning of optimization but will be eased back
+#     to the normal learning rate when steps>lr_delay_steps.
+#     :param conf: config subtree 'lr' or similar
+#     :param max_steps: int, the number of steps during optimization.
+#     :return HoF which takes step as input
+#     """
+
+#     def helper(step):
+#         if step < 0 or (lr_init == 0.0 and lr_final == 0.0):
+#             # Disable this parameter
+#             return 0.0
+#         if lr_delay_steps > 0:
+#             # A kind of reverse cosine decay.
+#             delay_rate = lr_delay_mult + (1 - lr_delay_mult) * np.sin(
+#                 0.5 * np.pi * np.clip(step / lr_delay_steps, 0, 1)
+#             )
+#         else:
+#             delay_rate = 1.0
+#         t = np.clip(step / max_steps, 0, 1)
+#         log_lerp = np.exp(np.log(lr_init) * (1 - t) + np.log(lr_final) * t)
+#         return delay_rate * log_lerp
+
+#     return helper
+
+# Rewritten to avoid pickling error
 def get_expon_lr_func(
         lr_init, lr_final, lr_delay_steps=0, lr_delay_mult=1.0, max_steps=1000000
 ):
@@ -56,23 +92,31 @@ def get_expon_lr_func(
     :param max_steps: int, the number of steps during optimization.
     :return HoF which takes step as input
     """
+    helper = ExponentialLR(lr_init, lr_final, lr_delay_steps, lr_delay_mult, max_steps)
+    return helper
 
-    def helper(step):
-        if step < 0 or (lr_init == 0.0 and lr_final == 0.0):
+class ExponentialLR:
+    def __init__(self, lr_init, lr_final, lr_delay_steps=0, lr_delay_mult=1.0, max_steps=1000000):
+        self.lr_init = lr_init
+        self.lr_final = lr_final
+        self.lr_delay_steps = lr_delay_steps
+        self.lr_delay_mult = lr_delay_mult
+        self.max_steps = max_steps
+
+    def __call__(self, step):
+        if step < 0 or (self.lr_init == 0.0 and self.lr_final == 0.0):
             # Disable this parameter
             return 0.0
-        if lr_delay_steps > 0:
+        if self.lr_delay_steps > 0:
             # A kind of reverse cosine decay.
-            delay_rate = lr_delay_mult + (1 - lr_delay_mult) * np.sin(
-                0.5 * np.pi * np.clip(step / lr_delay_steps, 0, 1)
+            delay_rate = self.lr_delay_mult + (1 - self.lr_delay_mult) * np.sin(
+                0.5 * np.pi * np.clip(step / self.lr_delay_steps, 0, 1)
             )
         else:
             delay_rate = 1.0
-        t = np.clip(step / max_steps, 0, 1)
-        log_lerp = np.exp(np.log(lr_init) * (1 - t) + np.log(lr_final) * t)
+        t = np.clip(step / self.max_steps, 0, 1)
+        log_lerp = np.exp(np.log(self.lr_init) * (1 - t) + np.log(self.lr_final) * t)
         return delay_rate * log_lerp
-
-    return helper
 
 
 def get_linear_noise_func(
--- utils/time_utils.py
+++ utils/time_utils.py
@@ -4,6 +4,32 @@ import torch.nn.functional as F
 from utils.rigid_utils import exp_se3
 
 
+# def get_embedder(multires, i=1):
+#     if i == -1:
+#         return nn.Identity(), 3
+
+#     embed_kwargs = {
+#         'include_input': True,
+#         'input_dims': i,
+#         'max_freq_log2': multires - 1,
+#         'num_freqs': multires,
+#         'log_sampling': True,
+#         'periodic_fns': [torch.sin, torch.cos],
+#     }
+
+#     embedder_obj = Embedder(**embed_kwargs)
+#     embed = lambda x, eo=embedder_obj: eo.embed(x) # AttributeError: Can't pickle local object 'get_embedder.<locals>.<lambda>'
+#     return embed, embedder_obj.out_dim
+
+# done this modification to avoid the pickling error from multiprocessing
+# AttributeError: Can't pickle local object 'get_embedder.<locals>.<lambda>'
+class EmbedderWrapper:
+    def __init__(self, embedder_obj):
+        self.embedder_obj = embedder_obj
+
+    def __call__(self, x):
+        return self.embedder_obj.embed(x)
+
 def get_embedder(multires, i=1):
     if i == -1:
         return nn.Identity(), 3
@@ -18,7 +44,7 @@ def get_embedder(multires, i=1):
     }
 
     embedder_obj = Embedder(**embed_kwargs)
-    embed = lambda x, eo=embedder_obj: eo.embed(x)
+    embed = EmbedderWrapper(embedder_obj)
     return embed, embedder_obj.out_dim
 
 
@@ -27,12 +53,38 @@ class Embedder:
         self.kwargs = kwargs
         self.create_embedding_fn()
 
+    # def create_embedding_fn(self):
+    #     embed_fns = []
+    #     d = self.kwargs['input_dims']
+    #     out_dim = 0
+    #     if self.kwargs['include_input']:
+    #         embed_fns.append(lambda x: x)
+    #         out_dim += d
+
+    #     max_freq = self.kwargs['max_freq_log2']
+    #     N_freqs = self.kwargs['num_freqs']
+
+    #     if self.kwargs['log_sampling']:
+    #         freq_bands = 2. ** torch.linspace(0., max_freq, steps=N_freqs)
+    #     else:
+    #         freq_bands = torch.linspace(2. ** 0., 2. ** max_freq, steps=N_freqs)
+
+    #     for freq in freq_bands:
+    #         for p_fn in self.kwargs['periodic_fns']:
+    #             embed_fns.append(lambda x, p_fn=p_fn, freq=freq: p_fn(x * freq))
+    #             out_dim += d
+
+    #     self.embed_fns = embed_fns
+    #     self.out_dim = out_dim
+
+    # got rid of lambda functions to avoid pickling error
+    # AttributeError: Can't pickle local object 'Embedder.create_embedding_fn.<locals>.<lambda>'
     def create_embedding_fn(self):
         embed_fns = []
         d = self.kwargs['input_dims']
         out_dim = 0
         if self.kwargs['include_input']:
-            embed_fns.append(lambda x: x)
+            embed_fns.append(self.identity)
             out_dim += d
 
         max_freq = self.kwargs['max_freq_log2']
@@ -45,15 +97,22 @@ class Embedder:
 
         for freq in freq_bands:
             for p_fn in self.kwargs['periodic_fns']:
-                embed_fns.append(lambda x, p_fn=p_fn, freq=freq: p_fn(x * freq))
+                self.freq = freq
+                self.p_fn = p_fn
+                embed_fns.append(self.periodic_fn)
                 out_dim += d
 
         self.embed_fns = embed_fns
         self.out_dim = out_dim
 
+    def identity(self, x):
+        return x
+
+    def periodic_fn(self, x):
+        return self.p_fn(x * self.freq)
+
     def embed(self, inputs):
         return torch.cat([fn(inputs) for fn in self.embed_fns], -1)
-
 
 class DeformNetwork(nn.Module):
     def __init__(self, D=8, W=256, input_ch=3, output_ch=59, multires=10, is_blender=False, is_6dof=False):
